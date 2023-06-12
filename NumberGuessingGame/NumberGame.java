1ae7c8ff4f9c460be3bf39a56e9a9820a3b566be
Anton Kaarakainen <anton.kaarakainen@outlook.com>
2023-06-12 11:52:19 +0900

    Fix double message when winning on fifth try

--- NumberGuessingGame/NumberGame.java
+++ NumberGuessingGame/NumberGame.java
@@ -7,6 +7,7 @@ public class NumberGame {
         Scanner scanner = new Scanner(System.in);
         int targetNumber = (int) (Math.random() * 90) + 10; // Generate a random two-digit number
         int numGuesses = 0;
+        boolean wonTheGame = false;
 
         System.out.println("Welcome to the Number Guessing Game!");
         System.out.println("You have 5 attempts to guess a two-digit number.");
@@ -14,12 +15,12 @@ public class NumberGame {
         while (numGuesses < 5) {
             System.out.print("Enter your guess: ");
             int guess = scanner.nextInt();
-            numGuesses++;
 
-            System.out.println(targetNumber);
+            numGuesses++;
 
             if (guess == targetNumber) {
                 System.out.println("Congratulations! You hit the target number.");
+                wonTheGame = true;
                 break;
             } else if (guess > targetNumber) {
                 System.out.println("Your guess is larger than the target number.");
@@ -32,7 +33,7 @@ public class NumberGame {
             }
         }
 
-        if (numGuesses >= 5) {
+        if (!wonTheGame && numGuesses >= 5) {
             System.out.println("Game over! You've reached the maximum number of guesses.");
             System.out.println("The target number was: " + targetNumber);
         }
